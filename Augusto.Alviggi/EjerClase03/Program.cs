191c473913dd6e3f43fc285272b3c2f81edcae24
augustoMA <augustoalviggi@gmail.com>
2018-03-27 12:12:11 -0300

    Add files via upload

---
+++ Augusto.Alviggi/EjerClase03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
+
+namespace PruebaGeometria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+}
+
---
+++ Augusto.Alviggi/EjerClase03/Punto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria
+{
+    class Punto
+    {
+        private int x;
+        private int y;
+
+        public int Getx()
+        {
+            int ejeX = 0;
+
+            ejeX = this.x;
+
+            return ejeX;
+        }
+            
+        public int GetY()
+        {
+            int ejeY = 0;
+
+            ejeY = this.y;
+
+           return ejeY;
+        }
+
+        public puntos(int )
+    }
+}
